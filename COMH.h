ad94caf823f8991efdb82a5ddb8766cc8e374f5c
omarelkawokgy <omarelkawokgy@gmail.com>
2016-02-07 09:09:22 +0200

    Create COMH.h

---
+++ COMH.h
@@ -0,0 +1,17 @@
+#ifndef COMH_H
+#define COMH_H
+#include "types.h"
+#include "CONF.h"
+
+typedef uint16 angle;
+
+class Comp
+{
+private:
+
+public:
+	Comp();
+
+	static angle ReadComp(void);
+};
+#endif
