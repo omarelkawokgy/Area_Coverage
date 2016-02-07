50aede290056ef708a3cb697c716ac7b616a4138
omarelkawokgy <omarelkawokgy@gmail.com>
2016-02-07 09:31:53 +0200

    Create ULSH.h

---
+++ ULSH.h
@@ -0,0 +1,21 @@
+#ifndef ULSH_H
+#define ULSH_H
+
+#include "CONF.h"
+#include "types.h"
+
+class ULSH
+{
+private:
+
+public:
+	ULSH();
+
+	static uint16 ULS_getLeftDist(void);
+
+	static uint16 ULS_getRightDist(void);
+
+	static L_R_Dist ULS_getDistanceboth(void);
+};
+
+#endif
