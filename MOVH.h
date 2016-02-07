5efc0f09a3a50ef97dda337bee06e782c5dedab7
omarelkawokgy <omarelkawokgy@gmail.com>
2016-02-07 09:31:06 +0200

    Create MOVH.h

---
+++ MOVH.h
@@ -0,0 +1,27 @@
+#ifndef MOVH_H
+#define MOVH_H
+
+#include "CONF.h"
+#include "Robot.h"
+
+class MOVE
+{
+private:
+
+public:
+	MOVE();
+
+	static void MoveForward(Robot* rob);
+
+	static void MoveBackward(Robot* rob);
+
+	static void MoveStop(Robot* rob);
+
+	static void MoveInitAngle(Robot* rob);
+
+	static void MoveTurn_CW(Robot* rob,uint8 TargetAngle);
+
+	static void MoveTurn_CCW(Robot* rob, uint8 TargetAngle);
+
+};
+#endif
