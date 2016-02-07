9ad52d13522ad67be2a25a259a3aaf7421bda251
omarelkawokgy <omarelkawokgy@gmail.com>
2016-02-07 09:08:02 +0200

    Create COMH.cpp

---
+++ COMH.cpp
@@ -0,0 +1,59 @@
+#include "COMH.h"
+#include<iostream>
+
+
+Comp::Comp()
+{
+
+}
+
+Heading Comp::ReadComp(void)
+{
+	uint16 Temp_angle;
+	Heading Angle;
+
+#ifdef ENABLE_SIMULATION
+	cout << "Angle from compass:" << endl;
+	cin >> Temp_angle;
+#else
+	comp Compass;
+	//raw data so its not scaled
+	MagnetometerRaw raw = Compass.ReadRawAxis();
+	MagnetometerScaled scaled = Compass.ReadScaledAxis();
+
+	float heading = atan2(raw.YAxis, raw.XAxis);
+
+	if (heading < 0)
+	{
+		heading += 2 * PI;
+	}
+
+	//convert radians to degrees
+	float headingDegrees = heading * 180 / M_PI;
+	//Serial.println(headingDegrees);
+
+	return headingDegrees;
+	//  Serial.println(" Degrees   \t");
+#endif
+	if ((Temp_angle > (NORTH_VALUE - ALLOWED_ANGLE_ERROR)) && (Temp_angle < (NORTH_VALUE + ALLOWED_ANGLE_ERROR)))
+	{
+		Angle = NORTH;
+	}
+	else if ((Temp_angle >(WEST_VALUE - ALLOWED_ANGLE_ERROR)) && (Temp_angle < (WEST_VALUE + ALLOWED_ANGLE_ERROR)))
+	{
+		Angle = WEST;
+	}
+	else if ((Temp_angle >(SOUTH_VALUE - ALLOWED_ANGLE_ERROR)) && (Temp_angle < (SOUTH_VALUE + ALLOWED_ANGLE_ERROR)))
+	{
+		Angle = SOUTH;
+	}
+	else if((Temp_angle > (NORTH_VALUE - ALLOWED_ANGLE_ERROR)) && (Temp_angle < (NORTH_VALUE + ALLOWED_ANGLE_ERROR)))
+	{
+		Angle = EAST;
+	}
+	else
+	{
+		/*TODO handling wrong calling while not being in 4 directions*/
+	}
+	return Angle;
+}
