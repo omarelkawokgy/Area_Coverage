26f1c5b7c8b8d28f41eeb9d325be3d61701079ab
omarelkawokgy <omarelkawokgy@gmail.com>
2016-02-07 09:33:03 +0200

    Create SIMU.cpp

---
+++ SIMU.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+#include "SIMU.h"
+#include "MapHandler.h"
+#include "CONF.h"
+
+using namespace std;
+
+
+
+void simu::printMap(Map m)
+{
+	for (uint8 i = 0; i < MAP_ROW; i++)
+	{
+		for (uint8 j = 0; j < MAP_COLUMN; j++)
+		{
+			cout << (int)m.room[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void simu::wheelTurn(Robot r)
+{
+	RobotPos position;
+
+	r.GetRobotPosition(&position);
+
+	if ((position.theta >= (NORTH_VALUE - ALLOWED_ANGLE_ERROR)) || (position.theta <= (NORTH_VALUE + ALLOWED_ANGLE_ERROR)))
+	{
+		/*TODO: increment according to orientation*/
+	}
+	else if ((position.theta >= (WEST_VALUE - ALLOWED_ANGLE_ERROR)) || (position.theta <= (WEST_VALUE + ALLOWED_ANGLE_ERROR)))
+	{
+		/*TODO: increment according to orientation*/
+	}
+	else if ((position.theta >= (SOUTH_VALUE - ALLOWED_ANGLE_ERROR)) || (position.theta <= (SOUTH_VALUE + ALLOWED_ANGLE_ERROR)))
+	{
+
+	}
+	else if ((position.theta >= (EAST_VALUE - ALLOWED_ANGLE_ERROR)) || (position.theta <= (EAST_VALUE + ALLOWED_ANGLE_ERROR)))
+	{
+		/*TODO: increment according to orientation*/
+	}
+	else
+	{
+		/*no increment or angle not right..... TODO: print angle recommended*/
+	}
+
+	r.UpdateRobotPosition(&position);
+}
+
+void simu::robotTurn(Robot r)
+{
+	RobotPos position;
+
+	r.GetRobotPosition(&position);
+
+	position.theta++;
+
+	r.UpdateRobotPosition(&position);
+}
