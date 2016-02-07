309c28b321ae8225fdac32591541e8e46cdfa405
omarelkawokgy <omarelkawokgy@gmail.com>
2016-02-07 09:35:33 +0200

    Create Robot.h

---
+++ Robot.h
@@ -0,0 +1,35 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include "types.h"
+
+#define INTIAL_X 0
+#define INTIAL_Y 0
+#define INTIAL_THETA 0
+
+class Robot
+{
+private:
+	uint8 X_Robot;
+	uint8 Y_Robot;
+	uint16 theta_Robot;
+
+	//default constructor
+	Robot(){}
+
+	//robot constructor singleton
+	Robot(RobotPos position);
+public:
+
+	//initial position where the robot starts its corrdinate and bearing
+	static Robot& initRobotPosition(RobotPos position);
+
+	//getting position of the robot
+	void GetRobotPosition(RobotPos* position);
+
+	//updating the position of the robot
+	void UpdateRobotPosition(RobotPos* position);
+
+};
+
+#endif
