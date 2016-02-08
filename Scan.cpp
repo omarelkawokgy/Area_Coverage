73886ecad7ccafaa0ff5ebdba6b9591ebeb13dae
omarelkawokgy <omarelkawokgy@gmail.com>
2016-02-08 11:22:39 +0200

    Update Scan.cpp

--- Scan.cpp
+++ Scan.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
 
+#include <iostream>
 #include "Scan.h"
 #include "ULSH.h"
 #include "MOVH.h"
-#include "OBJD.h"
+
 using namespace std;
 
 Scan& Scan::getInstanceScan()
 {
 	static Scan scan;
+#if 0 
 	for (uint8 i = 0; i < FULL_SCAN_NUM; i++)
 	{
 		scan.Distancelist[i].L_Distance = 0;
 		scan.Distancelist[i].R_Distance = 0;
 	}
+#endif
 	return scan;
 }
 
@@ -40,11 +42,23 @@ void Scan::CirclScanRoutine(Robot rob, L_R_Dist* scanlist)
 #endif
 }
 
-L_R_Points* LinearScanRoutine(Robot rob)
+void Scan::LinearScan(Point& newpoint, SensorID side, Robot rob)
 {
-	L_R_Dist scanlist;
+	uint16 ScanDist;
 
-	scanlist = ULSH::ULS_getDistanceboth();
-	static Point oldLeftPoint(LEFT_SENSOR,rob.GetRobotPosition(), scanlist);
-	static Point oldrightPoint(RIGHT_SENSOR, rob.GetRobotPosition(), scanlist);
+	if (side == RIGHT_SENSOR)
+	{
+		ScanDist = ULSH::ULS_getRightDist();
+		newpoint.CalPointPos(RIGHT_SENSOR, rob.GetRobotPosition(), ScanDist);
+	}
+	else if (side == LEFT_SENSOR)
+	{
+		ScanDist = ULSH::ULS_getLeftDist();
+		newpoint.CalPointPos(LEFT_SENSOR, rob.GetRobotPosition(), ScanDist);
+	}
+	else
+	{
+		/*do nothing*/
+	}
+	//return newPoint();
 }
