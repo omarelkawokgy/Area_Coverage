5002c4b4d1acd7610d84bea0d832b15a44ccd8c9
omarelkawokgy <omarelkawokgy@gmail.com>
2016-02-09 15:43:29 +0200

    Update MOVH.cpp

--- MOVH.cpp
+++ MOVH.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "MOVH.h"
+#include "SIMU.h"
+#include "COMH.h"
 
 using namespace std;
 
@@ -9,10 +11,52 @@ MOVE::MOVE()
 
 }
 
-void MOVE::MoveForward(Robot* rob)
+return_type MOVE::UpdatePosition(RobotPos* robposition)
+{
+	return_type ret = RET_NOT_OK;
+	Heading heading = Comp::ReadComp(robposition->theta);
+	if (heading == NORTH)
+	{
+		robposition->Y_pos--;
+		ret = RET_OK;
+	}
+	else if (heading == SOUTH)
+	{
+		robposition->Y_pos++;
+		ret = RET_OK;
+	}
+	else if (heading == WEST)
+	{
+		robposition->X_pos--;
+		ret = RET_OK;
+	}
+	else if (heading == EAST)
+	{
+		robposition->X_pos++;
+		ret = RET_OK;
+	}
+	else
+	{
+		ret = RET_NOT_OK;
+	}
+	return ret;
+}
+
+void MOVE::MoveForward(Robot& rob)
 {
 #ifdef ENABLE_SIMULATION
-
+	static uint8 Stepcounter = 0;
+	return_type Error_Check = RET_NOT_OK; 
+	RobotPos robposition = rob.GetRobotPosition();
+	Stepcounter++;
+	if((Stepcounter % 3) == 0)
+	{
+		Error_Check = UpdatePosition(&robposition);
+		if (Error_Check == RET_OK)
+		{
+			rob.UpdateRobotPosition(robposition);
+		}
+	}
 #else
 	digitalWrite(RIGHT_MOTOR_POSITIVE_PIN, HIGH);
 	digitalWrite(RIGHT_MOTOR_GROUND_PIN, LOW);
@@ -22,7 +66,7 @@ void MOVE::MoveForward(Robot* rob)
 
 }
 
-void MOVE::MoveBackward(Robot* rob)
+void MOVE::MoveBackward(Robot& rob)
 {
 #ifdef ENABLE_SIMULATION
 
@@ -34,7 +78,7 @@ void MOVE::MoveBackward(Robot* rob)
 #endif
 }
 
-void MOVE::MoveStop(Robot* rob)
+void MOVE::MoveStop(Robot& rob)
 {
 #ifdef ENABLE_SIMULATION
 
@@ -46,7 +90,7 @@ void MOVE::MoveStop(Robot* rob)
 #endif
 }
 
-void MOVE::MoveInitAngle(Robot* rob)
+void MOVE::MoveInitAngle(Robot& rob)
 {
 #ifdef ENABLE_SIMULATION
 
@@ -58,7 +102,7 @@ void MOVE::MoveInitAngle(Robot* rob)
 #endif
 }
 
-void MOVE::MoveTurn_CW(Robot* rob, uint8 TargetAngle)
+void MOVE::MoveTurn_CW(Robot& rob, uint8 TargetAngle)
 {
 #ifdef ENABLE_SIMULATION
 
@@ -70,7 +114,7 @@ void MOVE::MoveTurn_CW(Robot* rob, uint8 TargetAngle)
 #endif
 }
 
-void MOVE::MoveTurn_CCW(Robot* rob, uint8 TargetAngle)
+void MOVE::MoveTurn_CCW(Robot& rob, uint8 TargetAngle)
 {
 #ifdef ENABLE_SIMULATION
 
