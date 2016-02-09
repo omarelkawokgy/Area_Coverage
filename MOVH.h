105d6c19afb9b984a21ebdeb5b0e1a0bdee25c38
omarelkawokgy <omarelkawokgy@gmail.com>
2016-02-09 15:43:49 +0200

    Update MOVH.h

--- MOVH.h
+++ MOVH.h
@@ -7,21 +7,21 @@
 class MOVE
 {
 private:
-
+	static return_type UpdatePosition(RobotPos* robposition);
 public:
 	MOVE();
 
-	static void MoveForward(Robot* rob);
+	static void MoveForward(Robot& rob);
 
-	static void MoveBackward(Robot* rob);
+	static void MoveBackward(Robot& rob);
 
-	static void MoveStop(Robot* rob);
+	static void MoveStop(Robot& rob);
 
-	static void MoveInitAngle(Robot* rob);
+	static void MoveInitAngle(Robot& rob);
 
-	static void MoveTurn_CW(Robot* rob,uint8 TargetAngle);
+	static void MoveTurn_CW(Robot& rob, uint8 TargetAngle);
 
-	static void MoveTurn_CCW(Robot* rob, uint8 TargetAngle);
+	static void MoveTurn_CCW(Robot& rob, uint8 TargetAngle);
 
 };
 #endif
