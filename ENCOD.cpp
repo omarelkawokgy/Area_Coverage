0decc925e436cdc278856b3cba92f10afde72b2c
omarelkawokgy <omarelkawokgy@gmail.com>
2016-04-04 15:37:24 +0200

    Update ENCOD.cpp

--- ENCOD.cpp
+++ ENCOD.cpp
@@ -1,17 +1,46 @@
-#include "ENCOD.h"
+#ifndef ENCODER_H
+#define ENCODER_H
 
-left_encoder& left_encoder::getInstance(void)
+#include "CONF.h"
+
+class left_encoder
 {
-	static left_encoder(left_encoder_ticks);
+private:
+	ENCODER_UINT16 left_encoder_ticks;
 
-}
+	left_encoder(){}
 
-void left_encoder::incrementTick(void)
+	left_encoder(ENCODER_UINT16 left_encoder_local)
+	{
+		left_encoder_ticks = left_encoder_local;
+	}
+public:
+	static left_encoder& getInstance(ENCODER_UINT16 ticks);
+
+	void incrementTick(void);
+
+	void read_left_encoder_ticks(ENCODER_UINT16* ticks);
+};
+
+#ifndef SINGLE_ENCODER_ROBOT
+class right_encoder
 {
-	left_encoder_ticks++;
-}
+private:
+	ENCODER_UINT16 right_encoder_ticks;
 
-void left_encoder::read_left_encoder_ticks(ENCODER_UINT16* ticks)
-{
-	ticks = &left_encoder_ticks;
-}
+	right_encoder(){}
+
+	right_encoder(ENCODER_UINT16 right_encoder_local)
+	{
+		right_encoder_ticks = right_encoder_local;
+	}
+public:
+	static right_encoder& getInstance(void);
+
+	void incrementTick(void);
+
+	void read_right_encoder_ticks(void);
+};
+#endif
+
+#endif
